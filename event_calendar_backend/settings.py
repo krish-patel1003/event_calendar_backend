94e1db4cd0d8cae46c3925e30fdc5f126468efde
Krish Patel <patel.krish1003@gmail.com>
2024-12-05 15:03:28 +0530

    cors config changes

--- event_calendar_backend/settings.py
+++ event_calendar_backend/settings.py
@@ -30,6 +30,8 @@ DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
 
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS')
 
 # Application definition
