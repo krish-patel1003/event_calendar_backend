82bb2dc7880213498261e3f1b168acdb02a9a1d7
Krish Patel <patel.krish1003@gmail.com>
2024-12-05 14:45:57 +0530

    static file config changed

--- event_calendar_backend/settings.py
+++ event_calendar_backend/settings.py
@@ -129,6 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -159,5 +161,3 @@ SWAGGER_SETTINGS = {
         }
     },
 }
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
